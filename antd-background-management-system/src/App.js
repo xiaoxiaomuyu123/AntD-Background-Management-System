02366effce59547226593b017c125a50c1e1ad80
zengyan.wu <bandaridreams@163.com>
2020-08-27 11:40:51 +0800

    second

---
+++ antd-background-management-system/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import View from './pages/demo/view1'
+
+function App() {
+  return (
+    <>
+      <View />
+    </>
+  );
+}
+
+export default App;
---
+++ antd-background-management-system/src/pages/demo/view1.jsx
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react';
+import './view.less'
+
+class View extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <div className='box'>
+                    前段工作太多啦！
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default View
